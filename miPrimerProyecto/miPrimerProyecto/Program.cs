eadba2ad69ce42e775e378712c84a2e9a31b8de5
Luis Hernandez <sarco_luis@hotmail.com>
2024-10-15 20:23:11 -0600

    Creacion de mi primer proyecto

---
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miPrimerProyecto {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hola, chicos de Programacion Computacional I, Bienvenidos...");
+
+            Console.ReadLine(); // Para que no se cierre la consola
+        }
+    }
+}
