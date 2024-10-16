dd924afe6c9daca69e6a1bb0cf825ad15b2022c0
Luis Hernandez <sarco_luis@hotmail.com>
2024-10-15 20:29:51 -0600

    Suma de dos numeros introducidos por el usuario

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace miPrimerProyecto {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hola, chicos de Programacion Computacional I, Bienvenidos...");
+            //suma de dos numeros, introducidos por el usuario
+            Console.Write("Num 1: ");
+            int num1 = Int16.Parse(Console.ReadLine()); // "5" -> 5
+
+            Console.Write("Num 2: ");
+            int num2 = Int16.Parse(Console.ReadLine());
+
+            int suma = num1 + num2;
+            Console.WriteLine("La suma es: {0}", suma);
 
             Console.ReadLine(); // Para que no se cierre la consola
         }
