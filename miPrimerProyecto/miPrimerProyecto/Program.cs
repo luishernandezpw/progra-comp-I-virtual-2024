e78394217f386fb05cf49ec23725d7f8dd00438b
Luis Hernandez <sarco_luis@hotmail.com>
2024-10-17 19:27:58 -0600

    Mejora en el la calculadora basica

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -8,39 +8,46 @@ using System.Threading.Tasks;
 namespace miPrimerProyecto {
     internal class Program {
         static void Main(string[] args) {
-            //calculadora basica.
-            Console.WriteLine("Calculadora Basica");
-            Console.WriteLine("1. Suma");
-            Console.WriteLine("2. Resta");
-            Console.WriteLine("3. Multiplicacion");
-            Console.WriteLine("4. Division");
-            Console.Write("Opcion: ");
-            int opcion = int.Parse(Console.ReadLine());
+            String continuar = "s";
+            while (continuar == "s") {
+                //calculadora basica.
+                Console.WriteLine("Calculadora Basica");
+                Console.WriteLine("1. Suma");
+                Console.WriteLine("2. Resta");
+                Console.WriteLine("3. Multiplicacion");
+                Console.WriteLine("4. Division");
+                Console.WriteLine("5. Salir");
+                Console.Write("Opcion: ");
+                int opcion = int.Parse(Console.ReadLine());
 
-            Console.Write("Num 1: ");
-            double num1 = double.Parse(Console.ReadLine());
+                if (opcion == 5) {
+                    continuar = "n";
+                } else {
+                    Console.Write("Num 1: ");
+                    double num1 = double.Parse(Console.ReadLine());
 
-            Console.Write("Num 2: ");
-            double num2 = double.Parse(Console.ReadLine());
+                    Console.Write("Num 2: ");
+                    double num2 = double.Parse(Console.ReadLine());
 
-            double respuesta = 0;
-            String operacion = "";
+                    double respuesta = 0;
+                    String operacion = "";
 
-            if (opcion == 1) {
-                respuesta = num1 + num2;
-                operacion = "suma";
-            } else if (opcion == 2) {
-                respuesta = num1 - num2;
-                operacion = "resta";
-            } else if (opcion == 3) {
-                respuesta = num1 * num2;
-                operacion = "multiplicacion";
-            } else if (opcion == 4) {
-                respuesta = num1 / num2;
-                operacion = "division";
+                    if (opcion == 1) {
+                        respuesta = num1 + num2;
+                        operacion = "suma";
+                    } else if (opcion == 2) {
+                        respuesta = num1 - num2;
+                        operacion = "resta";
+                    } else if (opcion == 3) {
+                        respuesta = num1 * num2;
+                        operacion = "multiplicacion";
+                    } else if (opcion == 4) {
+                        respuesta = num1 / num2;
+                        operacion = "division";
+                    }
+                    Console.WriteLine("La {0} es igual a: {1}", operacion, respuesta);
+                }
             }
-            Console.WriteLine("La {0} es igual a: {1}",operacion, respuesta);
-            Console.ReadKey(); //Hacer una pausa
         }
     }
 }
