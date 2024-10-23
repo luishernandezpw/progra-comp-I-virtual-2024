daf17e00d60e305aecc020dbff11b28103d6e5be
Luis Hernandez <sarco_luis@hotmail.com>
2024-10-22 21:03:39 -0600

    Iniciamos con la creacion del CRUD de academica

--- academica/academica/Form1.Designer.cs
+++ academica/academica/Form1.Designer.cs
@@ -23,165 +23,20 @@
         /// el contenido de este método con el editor de código.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
-            this.lblNum1 = new System.Windows.Forms.Label();
-            this.txtNum1 = new System.Windows.Forms.TextBox();
-            this.txtNum2 = new System.Windows.Forms.TextBox();
-            this.lblNum2 = new System.Windows.Forms.Label();
-            this.lblRespuesta = new System.Windows.Forms.Label();
-            this.grbOpciones = new System.Windows.Forms.GroupBox();
-            this.optDivision = new System.Windows.Forms.RadioButton();
-            this.optMultiplicacion = new System.Windows.Forms.RadioButton();
-            this.optResta = new System.Windows.Forms.RadioButton();
-            this.optSuma = new System.Windows.Forms.RadioButton();
-            this.erpCalculadora = new System.Windows.Forms.ErrorProvider(this.components);
-            this.grbOpciones.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.erpCalculadora)).BeginInit();
             this.SuspendLayout();
-            // 
-            // lblNum1
-            // 
-            this.lblNum1.AutoSize = true;
-            this.lblNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNum1.Location = new System.Drawing.Point(218, 104);
-            this.lblNum1.Name = "lblNum1";
-            this.lblNum1.Size = new System.Drawing.Size(77, 24);
-            this.lblNum1.TabIndex = 1;
-            this.lblNum1.Text = "Num 1:";
-            // 
-            // txtNum1
-            // 
-            this.txtNum1.Location = new System.Drawing.Point(301, 104);
-            this.txtNum1.Name = "txtNum1";
-            this.txtNum1.Size = new System.Drawing.Size(100, 20);
-            this.txtNum1.TabIndex = 2;
-            // 
-            // txtNum2
-            // 
-            this.txtNum2.Location = new System.Drawing.Point(301, 147);
-            this.txtNum2.Name = "txtNum2";
-            this.txtNum2.Size = new System.Drawing.Size(100, 20);
-            this.txtNum2.TabIndex = 4;
-            // 
-            // lblNum2
-            // 
-            this.lblNum2.AutoSize = true;
-            this.lblNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNum2.Location = new System.Drawing.Point(218, 147);
-            this.lblNum2.Name = "lblNum2";
-            this.lblNum2.Size = new System.Drawing.Size(77, 24);
-            this.lblNum2.TabIndex = 3;
-            this.lblNum2.Text = "Num 2:";
-            // 
-            // lblRespuesta
-            // 
-            this.lblRespuesta.AutoSize = true;
-            this.lblRespuesta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRespuesta.Location = new System.Drawing.Point(12, 271);
-            this.lblRespuesta.Name = "lblRespuesta";
-            this.lblRespuesta.Size = new System.Drawing.Size(131, 24);
-            this.lblRespuesta.TabIndex = 5;
-            this.lblRespuesta.Text = "Respuesta: ?";
-            // 
-            // grbOpciones
-            // 
-            this.grbOpciones.Controls.Add(this.optDivision);
-            this.grbOpciones.Controls.Add(this.optMultiplicacion);
-            this.grbOpciones.Controls.Add(this.optResta);
-            this.grbOpciones.Controls.Add(this.optSuma);
-            this.grbOpciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.grbOpciones.Location = new System.Drawing.Point(12, 38);
-            this.grbOpciones.Name = "grbOpciones";
-            this.grbOpciones.Size = new System.Drawing.Size(195, 217);
-            this.grbOpciones.TabIndex = 6;
-            this.grbOpciones.TabStop = false;
-            this.grbOpciones.Text = "Opciones";
-            // 
-            // optDivision
-            // 
-            this.optDivision.AutoSize = true;
-            this.optDivision.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.optDivision.Location = new System.Drawing.Point(24, 181);
-            this.optDivision.Name = "optDivision";
-            this.optDivision.Size = new System.Drawing.Size(101, 28);
-            this.optDivision.TabIndex = 3;
-            this.optDivision.Text = "Division";
-            this.optDivision.UseVisualStyleBackColor = true;
-            this.optDivision.Click += new System.EventHandler(this.optDivision_Click);
-            // 
-            // optMultiplicacion
-            // 
-            this.optMultiplicacion.AutoSize = true;
-            this.optMultiplicacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.optMultiplicacion.Location = new System.Drawing.Point(24, 128);
-            this.optMultiplicacion.Name = "optMultiplicacion";
-            this.optMultiplicacion.Size = new System.Drawing.Size(156, 28);
-            this.optMultiplicacion.TabIndex = 2;
-            this.optMultiplicacion.Text = "Multiplicacion";
-            this.optMultiplicacion.UseVisualStyleBackColor = true;
-            this.optMultiplicacion.Click += new System.EventHandler(this.optMultiplicacion_Click);
-            // 
-            // optResta
-            // 
-            this.optResta.AutoSize = true;
-            this.optResta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.optResta.Location = new System.Drawing.Point(23, 82);
-            this.optResta.Name = "optResta";
-            this.optResta.Size = new System.Drawing.Size(80, 28);
-            this.optResta.TabIndex = 1;
-            this.optResta.Text = "Resta";
-            this.optResta.UseVisualStyleBackColor = true;
-            this.optResta.Click += new System.EventHandler(this.optResta_Click);
-            // 
-            // optSuma
-            // 
-            this.optSuma.AutoSize = true;
-            this.optSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.optSuma.Location = new System.Drawing.Point(23, 39);
-            this.optSuma.Name = "optSuma";
-            this.optSuma.Size = new System.Drawing.Size(81, 28);
-            this.optSuma.TabIndex = 0;
-            this.optSuma.Text = "Suma";
-            this.optSuma.UseVisualStyleBackColor = true;
-            this.optSuma.Click += new System.EventHandler(this.optSuma_Click);
-            // 
-            // erpCalculadora
-            // 
-            this.erpCalculadora.ContainerControl = this;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(538, 342);
-            this.Controls.Add(this.grbOpciones);
-            this.Controls.Add(this.lblRespuesta);
-            this.Controls.Add(this.txtNum2);
-            this.Controls.Add(this.lblNum2);
-            this.Controls.Add(this.txtNum1);
-            this.Controls.Add(this.lblNum1);
+            this.ClientSize = new System.Drawing.Size(628, 471);
             this.Name = "Form1";
             this.Text = "Saludo";
-            this.grbOpciones.ResumeLayout(false);
-            this.grbOpciones.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.erpCalculadora)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label lblNum1;
-        private System.Windows.Forms.TextBox txtNum1;
-        private System.Windows.Forms.TextBox txtNum2;
-        private System.Windows.Forms.Label lblNum2;
-        private System.Windows.Forms.Label lblRespuesta;
-        private System.Windows.Forms.GroupBox grbOpciones;
-        private System.Windows.Forms.RadioButton optDivision;
-        private System.Windows.Forms.RadioButton optMultiplicacion;
-        private System.Windows.Forms.RadioButton optResta;
-        private System.Windows.Forms.RadioButton optSuma;
-        private System.Windows.Forms.ErrorProvider erpCalculadora;
     }
 }
 
---
+++ academica/academica/db_conexion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data; //clase para conexion a base de datos
+using System.Data.SqlClient; //clase para conectarnos a SQL Server;
+
+namespace academica {
+    internal class db_conexion {
+        SqlConnection miConexion = new SqlConnection();//para conectarnos a la base de datos
+        SqlCommand miComando = new SqlCommand(); //para ejecutar comandos SQL en la base de datos
+        SqlDataAdapter miAdaptador = new SqlDataAdapter(); //Intermediario entre la base de datos y la aplicacion
+        DataSet ds = new DataSet(); //Ariquitectura de la base de datos en memoria RAM.
+
+        public db_conexion() {
+            miConexion.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\db_academica.mdf;Integrated Security=True";
+        }
+        public DataSet obtenerDatos() {
+            ds.Clear(); //limpiamos el dataset
+            miComando.Connection = miConexion; //asignamos la conexion al comando para pueda ejecutar las consultas
+            miComando.CommandText = "SELECT * FROM alumnos"; //consulta SQL
+            miAdaptador.Fill(ds, "alumnos"); //llenamos el dataset con la tabla alumnos
+
+            return ds;
+        }
+    }
+}
